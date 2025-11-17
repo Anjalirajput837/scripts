edd9859d35665648b45e7eb832f77af0776b8d4e
Anjali <anjalirajput5051@gmail.com>
2025-11-17 17:52:06 +0530

    Disk usage is normal

---
+++ diskusage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+THRESHOLD=80
+
+MOUNT_POINT="/c"
+
+USAGE=$(df -h | awk -v mount="$MOUNT_POINT" '$6 == mount {print $5}')
+
+echo "The usage percentage for $MOUNT_POINT is: $USAGE"
+
+if [ "$USAGE" -ge "$THRESHOLD" ]; then
+    echo "WARNING: Disk usage on / is at ${USAGE}% on $(hostname)!"
+    
+else
+    echo "Disk usage is normal (${USAGE}%)."
+fi
+
